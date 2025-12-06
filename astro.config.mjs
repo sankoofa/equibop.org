746401c76a4cf4680ace189cdb36552024fa6110
creations <creations@creations.works>
2025-12-06 08:17:49 -0500

    preview allowed hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ export default defineConfig({
         server: {
             allowedHosts: ['equibop.org'],
         },
+        preview: {
+            allowedHosts: ['equibop.org'],
+        },
     },
 
     integrations: [
